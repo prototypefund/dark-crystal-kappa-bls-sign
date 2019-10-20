af512d3745a1bc6622f5fd5119f7bc4a77aaa267
ameba23 <ameba23@systemli.org>
2019-10-20 13:47:48 +0200

    constructor, ready method

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ const kappaPrivate = require('kappa-private')
 const level = require('level')
 const Query = require('kappa-view-pull-query')
 const pull = require('pull-stream')
+const ThresholdSig = require('threshold-signatures')
+const path = require('path')
+const os = require('os')
 
 const LOCAL_FEED = 'local'
 const FEEDS = (dir) => path.join(dir, 'feeds')
 const VIEWS = (dir) => path.join(dir, 'views')
+const STORAGE = '.kappa-bls'
 
 class KappaBls {
-  constructor (opts = {}) {
+  constructor (threshold, numMembers, opts = {}) {
     this.indexesReady = false
     this.asymmetric = new kappaPrivate.Asymmetric()
 
     this.shares = {}
     this.peerNames = {}
     this.repliedTo = []
+    this.threshold = threshold
+    this.numMembers = numMembers
+    this.storage = opts.storage || path.join(os.homedir(), STORAGE)
 
     this.core = kappa(
       FEEDS(this.storage),
@@ -24,15 +31,18 @@ class KappaBls {
   }
 
   ready (cb) {
-    this.core.writer(LOCAL_FEED, (err, feed) => {
-      if (err) return cb(err)
-      feed.ready(() => {
-        this.localFeed = feed
-        this.key = feed.key
-        kappaPrivate.getSecretKey(FEEDS(this.storage), this.key, (err, secretKey) => {
-          if (err) return cb(err)
-          this.asymmetric.secretKey = secretKey
-          cb()
+    ThresholdSig.blsInit(() => {
+      this.member = ThresholdSig(this.threshold, this.numMembers)
+      this.core.writer(LOCAL_FEED, (err, feed) => {
+        if (err) return cb(err)
+        feed.ready(() => {
+          this.localFeed = feed
+          this.key = feed.key
+          kappaPrivate.getSecretKey(FEEDS(this.storage), this.key, (err, secretKey) => {
+            if (err) return cb(err)
+            this.asymmetric.secretKey = secretKey
+            cb()
+          })
         })
       })
     })
