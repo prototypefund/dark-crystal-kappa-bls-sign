643fd831c814c87b2ae4018de5bc42a571c16c01
ameba23 <ameba23@systemli.org>
2019-11-03 13:04:00 +0100

    test for error handing of missing member ids

--- test/index.test.js
+++ test/index.test.js
@@ -18,7 +18,10 @@ describe('basic', (context) => {
               assert.ok(schemas.isId(data), 'Published message is of correct form')
               assert.same('George', data.name, 'Name successfully published')
               assert.same(Object.keys(signer.member.members)[0], data.id, 'Published Id successfully stored')
-              next()
+              signer.publishContribution((err) => {
+                assert.ok(err, 'publishContribution returns an error when called without enough member Ids')
+                next()
+              })
             })
           })
         })
@@ -26,3 +29,11 @@ describe('basic', (context) => {
     })
   })
 })
+
+                // signer.localFeed.head((err, data) => {
+                //   console.log(err, data)
+                //   next()
+                // })
+                // const messages = signer.localFeed.createReadStream()
+                // messages.on('data', console.log)
+                // messages.on('end', next())
