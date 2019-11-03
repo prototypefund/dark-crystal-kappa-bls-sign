0959811d6ac37c6117dfa4f7191c6e3ffc7f4c4e
ameba23 <ameba23@systemli.org>
2019-11-03 12:17:21 +0100

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,25 @@
 const { describe } = require('tape-plus')
 const tmpDir = require('tmp').dirSync
+const schemas = require('../schemas')
 
 const KappaBlsSign = require('../')
 
 
 describe('basic', (context) => {
   context('key generation and signing', (assert, next) => {
-    const member = KappaBlsSign(3, 5, { storage: tmpDir().name })
-    member.ready(() => {
-      member.publishId('George', (err) => {
+    const signer = KappaBlsSign(3, 5, { storage: tmpDir().name })
+    signer.ready(() => {
+      signer.publishId('George', (err) => {
         assert.error(err, 'No error')
-        member.buildIndexes(() => {
-          member.queryIds(() => {
-            console.log(member.member.members) 
-            next()
+        signer.buildIndexes(() => {
+          signer.queryIds(() => {
+            signer.localFeed.get(0, (err, data) => {
+              assert.error(err, 'No error when reading from localFeed')
+              assert.ok(schemas.isId(data), 'Published message is of correct form')
+              assert.same('George', data.name, 'Name successfully published')
+              assert.same(Object.keys(signer.member.members)[0], data.id, 'Published Id successfully stored')
+              next()
+            })
           })
         })
       })
