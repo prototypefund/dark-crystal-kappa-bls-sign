43e60f5b7516910d9943335b50b28f2f9037aa6b
ameba23 <ameba23@systemli.org>
2019-11-08 12:35:00 +0100

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -1,39 +1,38 @@
 const { describe } = require('tape-plus')
 const tmpDir = require('tmp').dirSync
 const schemas = require('../schemas')
+const async = require('async')
 
 const KappaBlsSign = require('../')
 
-
 describe('basic', (context) => {
   context('key generation', (assert, next) => {
-    const signer = KappaBlsSign(3, 5, { storage: tmpDir().name })
-    signer.ready(() => {
-      signer.publishId('George', (err) => {
-        assert.error(err, 'No error')
+    const signers = []
+    const signerNames = ['Hedwig', 'George', 'Awet', 'Gertrude', 'Berni']
+    async.each(signerNames, (name, cb) => {
+      const signer = KappaBlsSign(3, 5, { storage: tmpDir().name })
+      signer.ready(() => {
+        signer.publishId(name, (err) => {
+          assert.error(err, 'No error')
+          signers.push(signer)
+          cb()
+        })
+      })
+    }, (err) => {
+      assert.error(err, 'No error')
+      async.each(signers, (signer, cb) => {
         signer.buildIndexes(() => {
           signer.queryIds(() => {
-            signer.localFeed.get(0, (err, data) => {
-              assert.error(err, 'No error when reading from localFeed')
-              assert.ok(schemas.isId(data), 'Published message is of correct form')
-              assert.same('George', data.name, 'Name successfully published')
-              assert.same(Object.keys(signer.member.members)[0], data.id, 'Published Id successfully stored')
-              signer.publishContribution((err) => {
-                assert.ok(err, 'publishContribution returns an error when called without enough member Ids')
-                next()
-              })
+            signer.publishContribution((err) => {
+              assert.error(err, 'No error on publishing contribution')
+              cb()
             })
           })
         })
       })
+    }, (err) => {
+      assert.error(err, 'No error')
+      next()
     })
   })
 })
-
-                // signer.localFeed.head((err, data) => {
-                //   console.log(err, data)
-                //   next()
-                // })
-                // const messages = signer.localFeed.createReadStream()
-                // messages.on('data', console.log)
-                // messages.on('end', next())
